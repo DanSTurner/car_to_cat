2dbab2564b5c729c3a09c9fa50025cbc249d7250
Dan S Turner <contact@dansturner.com>
2014-04-22 13:10:45 -0700

    catify

--- lib/car_to_cat/base.rb
+++ lib/car_to_cat/base.rb
@@ -1,7 +1,9 @@
 module CarToCat
   class Base
     def self.catify(string)
-      string.gsub(/(car)/, 'cat')
+      string.gsub(/(car)\b/i) do |match|
+        match[/r/] ? match.gsub(/r/, "t") : match.gsub(/R/, "T")
+      end
     end
   end
 end
--- spec/base_spec.rb
+++ spec/base_spec.rb
@@ -8,6 +8,18 @@ describe "CarToCat::Base" do
     it "should change car to cat" do
       expect(subject.catify("car")).to eq("cat")
     end
+
+    it "should replace all instances of car" do
+      expect(subject.catify("car car car")).to eq("cat cat cat")
+    end
+
+    it "should not change care to cate" do
+      expect(subject.catify("care")).to_not eq("cate")
+    end
+
+    it "should leave intact capitalization" do
+      expect(subject.catify("CAR")).to eq("CAT")
+    end
   end
 
 end
