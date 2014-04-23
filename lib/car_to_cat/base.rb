c41532c04e8030ad960e2779002b37b935a4ff26
Dan S Turner <contact@dansturner.com>
2014-04-23 10:21:22 -0700

    convert method

--- lib/car_to_cat/base.rb
+++ lib/car_to_cat/base.rb
@@ -1,9 +1,13 @@
 module CarToCat
   class Base
     def self.catify(string)
-      string.gsub(/(car)\b/i) do |match|
-        match[/r/] ? match.gsub(/r/, "t") : match.gsub(/R/, "T")
+      string.gsub(/(car)s?\b/i) do |match|
+        match[/r/] ? match.sub(/r/, "t") : match.sub(/R/, "T")
       end
+    end
+
+    def self.convert(input_file, output_file)
+      output_file.write(catify(input_file.string))
     end
   end
 end
--- spec/base_spec.rb
+++ spec/base_spec.rb
@@ -20,6 +20,55 @@ describe "CarToCat::Base" do
     it "should leave intact capitalization" do
       expect(subject.catify("CAR")).to eq("CAT")
     end
+
+    it "should match plural instances" do
+      expect(subject.catify("cars")).to eq("cats")
+    end
   end
 
+  describe "when handling a file" do
+    before(:each) do
+      @input_file = StringIO.new %{
+        In 2006, John Lasseter spoke about how they worked hard to make the \n
+        animation believable, saying: \"It took many months of trial and \n
+        error, and practicing test animation, to figure out how each car \n
+        moves and how their world works. Our supervising animators, Doug \n
+        Sweetland and Scott Clark, and the directing animators, Bobby Podesta \n
+        and James Ford Murphy, did an amazing job working with the animation \n
+        team to determine the unique movements for each character based on \n
+        its age and the type of car it was. Some cars are like sports cars \n
+        and they\'re much tighter in their suspension. Others are older \n
+        \'50s cars that are a lot looser and have more bounce to them. We \n
+        wanted to get that authenticity in there but also to make sure each \n
+        car had a unique personality. We also wanted each animator to be \n
+        able to put some of themself in the character and give it their own \n
+        spin. Every day in dailies, it was so much fun because we would see \n
+        things that we had never seen in our lives. The world of cars came \n
+        alive in a believable and unexpected way.\"[19]
+      }
+      @output_file = StringIO.new
+    end
+
+    it "should output a catified file" do
+      subject.convert(@input_file, @output_file)
+      @output_file.string.should eq(%{
+        In 2006, John Lasseter spoke about how they worked hard to make the \n
+        animation believable, saying: \"It took many months of trial and \n
+        error, and practicing test animation, to figure out how each cat \n
+        moves and how their world works. Our supervising animators, Doug \n
+        Sweetland and Scott Clark, and the directing animators, Bobby Podesta \n
+        and James Ford Murphy, did an amazing job working with the animation \n
+        team to determine the unique movements for each character based on \n
+        its age and the type of cat it was. Some cats are like sports cats \n
+        and they\'re much tighter in their suspension. Others are older \n
+        \'50s cats that are a lot looser and have more bounce to them. We \n
+        wanted to get that authenticity in there but also to make sure each \n
+        cat had a unique personality. We also wanted each animator to be \n
+        able to put some of themself in the character and give it their own \n
+        spin. Every day in dailies, it was so much fun because we would see \n
+        things that we had never seen in our lives. The world of cats came \n
+        alive in a believable and unexpected way.\"[19]
+      })
+    end
+  end
 end
